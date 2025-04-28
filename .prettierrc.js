2b7a9e714e42d3f570ad49ed0177553bd2aa8ff2
朱天文 <zhutianwen@szlanyou.com>
2025-04-28 17:52:47 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+}
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,10 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+
+
+// 定义 从数据库返回的数据类型
+
 export type User = {
   id: string;
   name: string;
@@ -21,8 +25,6 @@ export type Invoice = {
   customer_id: string;
   amount: number;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
 
